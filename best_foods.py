2d9fbe15c56bd897b60a355303ff787248a97776
LanpherKa <kapenniman@philasd.org>
2025-04-25 11:25:10 -0400

    Update best_foods.py

--- best_foods.py
+++ best_foods.py
@@ -2,4 +2,19 @@
 #Print the list as a numbered list
 #Print the list in alphabetical order (you can create a new list or change the original one)
 
-favorite_foods = []
+favorite_foods = ["Chocolate Cake", "Yaki Udon", "Bar-B-Q Short Ribs", "Pizza", "Shrimp Tempura Roll", "Chocolate Chip Cookie", "Pesto Pasta Salad", "Fried Chicken", "Burger", "Peanut Butter Cup"  ]
+
+print("FOODS IN ORDER OF PREFERENCE")
+print()
+for i in range(len(favorite_foods)):
+    print(str(i + 1) +" "+ favorite_foods[i])
+
+alphabetical = favorite_foods
+alphabetical.sort()
+print()
+print("FOODS IN ALPHABETICAL ORDER")
+print()
+
+for food in alphabetical:
+    print("- " + food)
+    
