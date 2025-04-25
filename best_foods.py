87d399a0c8469be71dab2efbbd982e944c637ed0
LanpherKa <kapenniman@philasd.org>
2025-04-25 10:57:17 -0400

    Create best_foods.py

---
+++ best_foods.py
@@ -0,0 +1,5 @@
+#Create a list of your 10 favorite foods in order from best to least best.
+#Print the list as a numbered list
+#Print the list in alphabetical order (you can create a new list or change the original one)
+
+favorite_foods = []
