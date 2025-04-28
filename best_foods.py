b81769f017bbde4bbcfbce2d0e9bad5e14042774
Elnur Ermekov <81921000+eerm777@users.noreply.github.com>
2025-04-28 10:37:14 -0400

    Update best_foods.py
    Version 2

--- best_foods.py
+++ best_foods.py
@@ -1,5 +1,53 @@
-#Create a list of your 10 favorite foods in order from best to least best.
-#Print the list as a numbered list
-#Print the list in alphabetical order (you can create a new list or change the original one)
+favorite_foods = ["Steak", "Manti", "Loaded fries", "Shashlyk", "Shawerma", "Lagman", "Strawberry Cheescake Milkshake", "Brizol"]
 
-favorite_foods = []
+print("FOODS IN ORDER OF PREFERENCE")
+print()
+for i in range(len(favorite_foods)):
+    print(str(i + 1) + " " + favorite_foods[i])
+
+alphabetical = favorite_foods
+alphabetical.sort()
+print()
+print("FOODS IN ALPHABETICAL ORDER")
+print()
+
+for food in alphabetical:
+    print("- " + food)
+
+print("")
+
+favorite_places = ["Bishkek", "Issyk-kul", "Gym", "Home", "Cyber-Club", "Beach", "Bathroom", "\'School\'"]
+
+print("PLACES IN ORDER OF PREFERENCE")
+print()
+for i in range(len(favorite_places)):
+    print(str(i + 1) + " " + favorite_places[i])
+
+alphabetical_places = favorite_places
+alphabetical_places.sort()
+print()
+print("PLACES IN ALPHABETICAL ORDER")
+print()
+
+for place in alphabetical_places:
+    print("- " + place)
+
+
+print("")
+
+
+favorite_sports = ["Soccer", "Volleyball", "Table Tennis", "Boxing", "Wrestling", "MMA", "Football", "Basketball"]
+
+print("SPORTS IN ORDER OF PREFERENCE")
+print()
+for i in range(len(favorite_sports)):
+    print(str(i + 1) + " " + favorite_sports[i])
+
+alphabetical_sports = favorite_sports
+alphabetical_sports.sort()
+print()
+print("SPORTS IN ALPHABETICAL ORDER")
+print()
+
+for sport in alphabetical_sports:
+    print("- " + sport)
