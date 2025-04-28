06eba9b7226810794232276f12b540edac4ddfda
LanpherKa <kapenniman@philasd.org>
2025-04-28 10:37:05 -0400

    Update best_foods.py
    Version 2

--- best_foods.py
+++ best_foods.py
@@ -2,4 +2,17 @@
 #Print the list as a numbered list
 #Print the list in alphabetical order (you can create a new list or change the original one)
 
-favorite_foods = [ ]
+favorite_foods = ["Chocolate Cake", "Pasta", "Turkey Sandwich",
+                  "Pumpkin Pie", "Roast Beef", "Pizza", "Potato Chips",
+                  "Popcorn", "Cookies", "Broccoli"]
+
+for i, food in enumerate(favorite_foods):
+    print(f"{i+1}. {food}")
+
+sorted_list =favorite_foods
+sorted_list.sort()
+
+
+
+
+
